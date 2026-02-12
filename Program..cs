55b7929063573fde66b7409f4e5a16e371aa9833
Morales <Ezra.2158527@ad.ccsd.net>
2026-02-12 14:24:58 -0800

    send help aaaaaaaaaaaaaaaa

---
+++ Program..cs
@@ -0,0 +1,15 @@
+
+namespace terminalfactory;
+
+class Factory // factory data
+{
+    
+}
+class Game
+{
+    public static void Main()
+    {
+        Factory data = new Factory();
+        
+    }
+}
