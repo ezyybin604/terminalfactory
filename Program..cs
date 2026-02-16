da5a3f65106f5ac3b207ada9cfb6abb2b79e1697
ezyybin604 <ezra@no.com>
2026-02-16 15:23:16 -0800

    made framework for generatechunk function

--- Program..cs
+++ Program..cs
@@ -1,33 +1,67 @@
 
+using System;
 namespace terminalfactory;
 
+// Inspired a little bit by https://www.youtu.be/cZYNADOHhVY :)
+
+/*
+Tile types:
+f: finite resource
+i: infinite resource
+' ': grass/empty
+1: machine block tier 1
+2: machine block tier 2
+3: machine block tier 3
+*: energy port
++: add/input
+-: get/output
+=: pipe
+#: cable
+]: trough
+@: world interactor
+M: machine
+b: bush
+*/
+public struct Tile
+{
+    public char type;
+    public string subtype; ///machine type/resource in tile/fruit type
+    public int prog; // Amount of tile in tile/machine progress/stored amount/energy amount
+    public string item; // machine output/storage type
+}
+public struct Chunk
+{
+    public int x;
+    public int y;
+    public Tile[][] data;
+}
 class Factory // factory data
 {
     public string savefile = "defualt.tf";
+    int chunkSize = 16;
+    // [x][y]
+    Dictionary<int, Dictionary<int, Chunk>> world = new Dictionary<int, Dictionary<int, Chunk>>();
+    void generateChunk(int x, int y)
+    {
+        Chunk chunk = new Chunk();
+        chunk.x = x;
+        chunk.y = y;
+        // Generate chunk data herre
+        if (!world.Keys.Contains(x))
+        {
+            world.Add(x, new Dictionary<int, Chunk>());
+        }
+        if (!world[x].Keys.Contains(y))
+        {
+            world[x].Add(y, chunk);
+        }
+    }
     // add world storage, uhhh figure that out later
     // also make a worldgen function
     // add world tick function to tick the world
 }
 class Game
 {
-    void AskForWindowChange(int width, int height)
-    {
-        Console.Clear();
-        try
-        {
-            Console.SetWindowSize(width, height);
-        }
-        catch
-        {
-            while ((Console.WindowWidth != width) || (Console.WindowHeight != height))
-            {
-                Console.WriteLine(String.Format("Window size tried to change, but an error occured.\nCan you change it to {0}x{1}?", width, height));
-                Console.WriteLine(String.Format("Current size: {0}x{1}", Console.WindowWidth, Console.WindowHeight));
-                Console.WriteLine("Press key when ready to try again.");
-                Console.ReadKey();
-            }
-        }
-    }
     public static void Main()
     {
         Game game = new Game();
@@ -56,7 +90,7 @@ Nobody follows, so to keep secrecy while you travel.
 (Press ENTER to start)");
             }
             Console.ReadLine();
-            game.AskForWindowChange(120,40);
+            
         }
     }
 }
