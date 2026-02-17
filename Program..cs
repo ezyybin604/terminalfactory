5fbbb4ec9d49b5c4534735a2e458675c86c7449b
ezyybin604 <ezra@no.com>
2026-02-16 22:28:37 -0800

    hopefully i can start ui stuff tomorrow

--- Program..cs
+++ Program..cs
@@ -4,24 +4,6 @@ namespace terminalfactory;
 
 // Inspired a little bit by https://www.youtu.be/cZYNADOHhVY :)
 
-/*
-Tile types:
-f: finite resource
-i: infinite resource
-' ': grass/empty
-1: machine block tier 1
-2: machine block tier 2
-3: machine block tier 3
-*: energy port
-+: add/input
--: get/output
-=: pipe
-#: cable
-]: trough
-@: world interactor
-M: machine
-b: bush
-*/
 public struct Tile
 {
     public char type;
@@ -29,44 +11,57 @@ public struct Tile
     public int prog; // Amount of tile in tile/machine progress/stored amount/energy amount
     public string item; // machine output/storage type
 }
+public struct Point
+{
+    public int x;
+    public int y;
+    public Point(int ix, int iy)
+    {
+        x = ix;
+        y = iy;
+    }
+    public Point()
+    {
+        x = 0;
+        y = 0;
+    }
+}
 public struct Chunk
 {
     public int x;
     public int y;
+    // [x][y]
     public Tile[][] data;
 }
-class Factory // factory data
-{
-    public string savefile = "defualt.tf";
-    int chunkSize = 16;
-    // [x][y]
-    Dictionary<int, Dictionary<int, Chunk>> world = new Dictionary<int, Dictionary<int, Chunk>>();
-    void generateChunk(int x, int y)
-    {
-        Chunk chunk = new Chunk();
-        chunk.x = x;
-        chunk.y = y;
-        // Generate chunk data herre
-        if (!world.Keys.Contains(x))
-        {
-            world.Add(x, new Dictionary<int, Chunk>());
-        }
-        if (!world[x].Keys.Contains(y))
-        {
-            world[x].Add(y, chunk);
-        }
-    }
-    // add world storage, uhhh figure that out later
-    // also make a worldgen function
-    // add world tick function to tick the world
-}
+
 class Game
 {
+    Point scroll = new Point();
+    Factory factory = new Factory();
+    void generateNeeded()
+    {
+        int w = (int)Math.Ceiling((double)(Console.WindowWidth/factory.chunkSize));
+        int h = (int)Math.Ceiling((double)((Console.WindowHeight-2)/factory.chunkSize));
+        for (int x=0;x<w;x++)
+        {
+            for (int y=0;y<h;y++)
+            {
+                factory.generateChunk(x, y);
+            }
+        }
+    }
+    void displayStuff()
+    {
+        Console.Clear();
+        Console.WriteLine("Insert tips/controls/tutorial");
+        Console.WriteLine(new string('~', Console.WindowWidth));
+        // uhhh finish world render stuff later
+    }
     public static void Main()
     {
         Game game = new Game();
-        Factory factory = new Factory();
-        if (!File.Exists(factory.savefile))
+        game.factory = new Factory();
+        if (!File.Exists(game.factory.savefile))
         {
             Console.WriteLine("No savefile found.");
             Console.Write("Skip intro? (y/n):");
@@ -90,7 +85,9 @@ Nobody follows, so to keep secrecy while you travel.
 (Press ENTER to start)");
             }
             Console.ReadLine();
-            
+            game.generateNeeded();
+            game.displayStuff();
+            Console.ReadLine();
         }
     }
 }
